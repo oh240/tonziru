dd7d54cb3b219341152f7ed388f574488a41fee9
Nishio Takuya <littlecub240@gmail.com>
2013-12-18 19:01:58 +0900

    facebook add

---
+++ app/Config/facebook.php
@@ -0,0 +1,14 @@
+<?php
+/**
+  * Get an api_key and secret from facebook and fill in this content.
+  * save the file to app/Config/facebook.php
+  */
+  $config = array(
+  	'Facebook' => array(
+  		'appId' => '510530838968620',
+  		'apiKey' => '510530838968620',
+  		'secret' => 'efc24f3b0d8f10008b0bee3df6b38b23',
+  		'cookie' => true,
+  		'locale' => 'ja_JP',
+  		)
+  	);
